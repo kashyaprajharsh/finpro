ebe97058e2e19d96d0d3cd77f2d4df9e07660fc8
Reetu Raj Harsh <54144236+kashyaprajharsh@users.noreply.github.com>
2024-03-03 23:19:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,11 +22,10 @@ from langkit import response_hallucination # alternatively use 'light_metrics'
 import whylogs as why
 from whylogs.experimental.core.udf_schema import udf_schema
 
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
+hide_github_icon = “”"
+
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+“”"
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 load_dotenv()
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
