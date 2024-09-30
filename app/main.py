abab52b68f58da990b2794afc4d856e63a55f64b
kashyaprajharsh <reeturajharsh@gmail.com>
2024-09-30 16:52:59 +0530

    refatored code with api

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ if not os.path.exists(os.path.join(nltk_data_path, "sentiment", "vader_lexicon.t
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api.routes import router as api_router
+from app.api.routes import router as api_router
 from config import settings
 
 app = FastAPI()
