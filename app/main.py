8bb9d7fa44518bee0764be9fad391c81e95809cb
kashyaprajharsh <reeturajharsh@gmail.com>
2024-09-30 16:40:36 +0530

    refatored code with api

--- app/main.py
+++ app/main.py
@@ -24,4 +24,6 @@ app.include_router(api_router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
